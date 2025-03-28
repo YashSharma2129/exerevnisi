111ba7dca438e60acc0b66c11802149794093a78
Yash <yash25578@gmail.com>
2025-03-28 20:10:40 +0530

    FIX

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "../footer";
 import { useNavigate } from "react-router-dom";
 
-const Blog = () => {
+const BlogPage = () => {
 
     const navigate = useNavigate();
 
@@ -124,4 +124,4 @@ const Blog = () => {
     );
 };
 
-export default Blog;
+export default BlogPage;
